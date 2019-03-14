43dc0cb2532cd23430431abb59aec39d9ac6109a
facundoq <facundoq@gmail.com>
2019-03-14 14:13:27 -0300

    added info to setup.py, gitignore and version to __init__.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+
 REQUIRED_PACKAGES =[
     'scikit-video',
     'matplotlib',
@@ -7,22 +8,33 @@ REQUIRED_PACKAGES =[
     'opencv-python',
     'tensorflow',   
 ]
+
+project_urls={
+    "Bug Tracker": "'https://github.com/midusi/sign_language_datasets/',issues",
+    "Documentation": "'https://github.com/midusi/sign_language_datasets/wiki',",
+    "Source Code": "'https://github.com/midusi/sign_language_datasets",
+}
+
 setup(
-    name='sign-language-datasets',
+    name='sldatasets',
     version='0.0.1',
-    author='pablo kepes',
+    author='Pablo Kepes and Facundo Quiroga',
+    author_email="signlanguagedatasets@gmail.com",
     description=(
         'A single library to (down)load all existing sign language video datasets.'),
     license='GNU',
-    keywords='sign-language',
-    url='https://github.com/midusi/sign_language_datasets/',
-    download_url='https://github.com/midusi/sign_language_datasets/tarball/0.1',
+    keywords='sign-language sign language dataset download load video',
+    project_urls=project_urls,
     packages=find_packages(),
     install_requires=REQUIRED_PACKAGES,
+    #zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GNU License',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
-        'Topic :: Manager'
+        'Topic :: Manager',
+        'Topic :: Sign Language',
+        'Topic :: Datasets'
     ]
 )
